8d199d577604c8ea8bcbd0298a3f8b0f7e5a701e
Daniel Beeke <mail@danielbeeke.nl>
2023-12-26 12:47:37 +0100

    removing flatfilestore

--- packages/FlatFileStore/src/core/helpers/namespaces.ts
+++
@@ -1,7 +0,0 @@
-import { namespace } from '../../deps.ts'
-
-export const schema = namespace('https://schema.org/')
-export const rdfs = namespace('http://www.w3.org/2000/01/rdf-schema#')
-export const rdf = namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-export const ex = namespace('http://example.com/')
-export const ffs = namespace('https://centergraph.danielbeeke.nl/ffs#')
