ea3df98a846c971bb3e4457d56add32a94aceea5
Daniel Beeke <mail@danielbeeke.nl>
2023-12-29 19:06:41 +0100

    fixing meta test for blank node viewer

--- packages/shacl-renderer/lib/components/widgets/viewers/BlankNodeViewer/meta.test.ts
+++ packages/shacl-renderer/lib/components/widgets/viewers/BlankNodeViewer/meta.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { score } from './meta'
-import { schema } from '@/helpers/namespaces'
+import { schema, sh } from '@/helpers/namespaces'
 import { prepareScoreTest } from '@/helpers/prepareTestScore'
 
 test('it scores', async () => {
@@ -13,4 +13,16 @@ test('it does not score', async () => {
   const { dataPointer, shaclPointer } = await prepareScoreTest(schema('givenName'))
   const givenScore = score(dataPointer, shaclPointer)
   expect(givenScore).toBe(-1)
+})
+
+test.only('it score for without', async () => {
+  const { dataPointer, shaclPointer } = await prepareScoreTest(schema('address'))
+  const givenScore1 = score(dataPointer, shaclPointer)
+  expect(givenScore1).toBe(1)
+
+  shaclPointer.deleteOut(sh('nodeKind'))
+  dataPointer.deleteOut(schema('address'))
+
+  const givenScore2 = score(dataPointer.node(), shaclPointer)
+  expect(givenScore2).toBe(undefined)
 })
--- packages/shacl-renderer/lib/components/widgets/viewers/LiteralViewer/meta.test.ts
+++ packages/shacl-renderer/lib/components/widgets/viewers/LiteralViewer/meta.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { score } from './meta'
-import { schema } from '@/helpers/namespaces'
+import { schema, sh } from '@/helpers/namespaces'
 import { prepareScoreTest } from '@/helpers/prepareTestScore'
 
 test('it scores', async () => {
@@ -13,4 +13,11 @@ test('it does not score', async () => {
   const { dataPointer, shaclPointer } = await prepareScoreTest(schema('address'))
   const givenScore = score(dataPointer, shaclPointer)
   expect(givenScore).toBe(undefined)
+})
+
+test('it scores on datatype', async () => {
+  const { dataPointer, shaclPointer } = await prepareScoreTest(schema('givenName'))
+  shaclPointer.deleteOut(sh('datatype'))
+  const givenScore = score(dataPointer, shaclPointer)
+  expect(givenScore).toBe(1)
 })
